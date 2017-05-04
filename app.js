163d9b69dde07ea740be9a8e558be2a8e9540d4d
LeXeL <lexelEZ@gmail.com>
2017-05-04 16:47:50 -0500

    Added waitjs support

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
+require('waitjs');
+
 const Descuentos = require('./crawlers/Descuentos');
 const Nuevos = require('./crawlers/Nuevos');
 
 
-Descuentos.DescuentosDelMes('http://yoytec.com/index.php').then((res)=>{
-  console.log('Nuevos Productos en el mes!');
-  console.log(JSON.stringify(res,null,4));
-  console.log('Descuentos agregados!');
-  return Nuevos.NuevosDelMes('http://yoytec.com/index.php');
-}).then((res)=>{
-  console.log(JSON.stringify(res,null,4))
-}).catch((e)=>{
-  console.log(e);
+repeat('5 min', () => {
+    Descuentos.DescuentosDelMes('http://yoytec.com/index.php').then((res) => {
+        console.log('Nuevos Productos en el mes!');
+        console.log(JSON.stringify(res, null, 4));
+        console.log('Descuentos agregados!');
+        return Nuevos.NuevosDelMes('http://yoytec.com/index.php');
+    }).then((res) => {
+        console.log(JSON.stringify(res, null, 4))
+    }).catch((e) => {
+        console.log(e);
+    });
 });
