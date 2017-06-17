144b8864180d7f69fa9ea7f7e1ac6a8bb00d099a
Diego Rodriguez <lexelez@gmail.com>
2017-06-16 19:20:53 -0500

    Added support for new website

--- crawlers/News.js
+++ crawlers/News.js
@@ -2,38 +2,27 @@ const express = require('express')
 const request = require('request')
 const cheerio = require('cheerio')
 
-var GetNews = (url)=> {
-  var newObj = []
+var GetNews = (link)=> {
+  let newObj = []
   return new Promise(function(resolve, reject) {
-    request(url, (e, res) => {
+    request(link, (e, res) => {
       if (e) {
         reject('Unable to connect to yoytec server for News')
       }
       var $ = cheerio.load(res.body)
-      $('.name').each((i,e)=>{
-        let url = e.children[0].children[0].attribs.href
-        let name = e.children[0].children[0].children[0].data
-        let price = e.parent
-        console.log(price)
+      $('.span2_20').each((i,e)=>{
+        var url = e.children[1].children[1].children[0].attribs.href
+        var img ='https://yoytec.com/'+e.children[1].children[1].children[0].children[0].attribs.src
+        var name = e.children[1].children[1].children[0].children[0].attribs.title
+        var price = e.children[1].children[7].children[5].children[1].children[1].children[1].children[0].data
+        var news = {
+              name,
+              img,
+              url,
+              price
+            }
+            newObj.push(news)
       })
-
-
-      // $('td .smallText').each((i, e) => {
-      //   if (e.name === 'td' && e.children[0].name === 'a') {
-      //     var img = 'http://yoytec.com/' + e.children[0].children[0].attribs.src
-      //     var name = e.children[0].children[0].attribs.title
-      //     var url = e.children[0].attribs.href
-      //     var price = e.children[0].next.next.next.next.children[0].data
-      //     // console.log(name, url, img)
-      //     var news = {
-      //       name,
-      //       img,
-      //       url,
-      //       price
-      //     }
-      //     newObj.push(news)
-      //   }
-      // })
       resolve(newObj)
     })
   })
